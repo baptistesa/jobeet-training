138760ed59f35bff8ab7228cfcd8689d66016222
Baptiste Saint-André <baptiste.saintandre@gmail.com>
2020-10-18 00:51:59 +0200

    Base app

--- src/app/app.module.ts
+++ src/app/app.module.ts
@@ -4,15 +4,21 @@ import { NgModule } from '@angular/core';
 import { AppRoutingModule } from './app-routing.module';
 import { AppComponent } from './app.component';
 import { BrowserAnimationsModule } from '@angular/platform-browser/animations';
+import { CustomMaterialModule } from './core/material.module';
+import { NavigationComponent } from './navigation/navigation.component';
+import { HomeComponent } from './home/home.component';
 
 @NgModule({
   declarations: [
-    AppComponent
+    AppComponent,
+    NavigationComponent,
+    HomeComponent
   ],
   imports: [
     BrowserModule,
     AppRoutingModule,
-    BrowserAnimationsModule
+    BrowserAnimationsModule,
+    CustomMaterialModule
   ],
   providers: [],
   bootstrap: [AppComponent]
---
+++ src/app/core/material.module.ts
@@ -0,0 +1,69 @@
+import { NgModule } from "@angular/core";
+import { CommonModule } from '@angular/common';
+import { BrowserAnimationsModule } from '@angular/platform-browser/animations';
+
+import { MatSidenavModule } from "@angular/material/sidenav";
+import { MatToolbarModule } from "@angular/material/toolbar";
+import { MatButtonModule } from "@angular/material/button";
+import { MatListModule } from "@angular/material/list";
+import { MatIconModule } from "@angular/material/icon";
+import { MatStepperModule } from "@angular/material/stepper";
+import { MatFormFieldModule } from "@angular/material/form-field";
+import { MatInputModule } from "@angular/material/input"
+import { MatTreeModule } from '@angular/material/tree';
+import { MatSelectModule } from "@angular/material/select";
+import { MatTabsModule } from "@angular/material/tabs";
+import { MatExpansionModule } from "@angular/material/expansion";
+import { MatCheckboxModule } from "@angular/material/checkbox";
+import { MatDialogModule } from '@angular/material/dialog';
+
+
+import { CdkStepperModule } from '@angular/cdk/stepper';
+
+import { DragDropModule } from "@angular/cdk/drag-drop";
+
+
+
+@NgModule({
+    imports: [
+        CommonModule,
+        MatSidenavModule,
+        BrowserAnimationsModule,
+        MatToolbarModule,
+        MatButtonModule,
+        MatListModule,
+        MatIconModule,
+        MatStepperModule,
+        MatFormFieldModule,
+        MatInputModule,
+        MatTreeModule,
+        CdkStepperModule,
+        MatSelectModule,
+        MatTabsModule,
+        MatExpansionModule,
+        DragDropModule,
+        MatCheckboxModule,
+        MatDialogModule
+    ],
+    exports: [
+        CommonModule,
+        MatSidenavModule,
+        BrowserAnimationsModule,
+        MatToolbarModule,
+        MatButtonModule,
+        MatListModule,
+        MatIconModule,
+        MatStepperModule,
+        MatFormFieldModule,
+        MatInputModule,
+        MatTreeModule,
+        CdkStepperModule,
+        MatSelectModule,
+        MatTabsModule,
+        MatExpansionModule,
+        DragDropModule,
+        MatCheckboxModule,
+        MatDialogModule
+    ],
+})
+export class CustomMaterialModule { }
